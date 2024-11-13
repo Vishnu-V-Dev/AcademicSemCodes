8abfd6b55ddfdc9baf2dce48ce5216f66e785af3
75hrtx <vishnu.navaneetham@outlook.com>
2024-11-13 11:27:01 +0530

    Update Postfix.c

--- C/Postfix.c
+++ C/Postfix.c
@@ -3,9 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
-// Function to return precedence of operators
 int prec(char c) {
-    
     if (c == '^')
         return 3;
     else if (c == '/' || c == '*')
@@ -16,38 +14,28 @@ int prec(char c) {
         return -1;
 }
 
-// Function to perform infix to postfix conversion
 void infixToPostfix(char* exp) {
-    int len = strlen(exp);
-    char result[len + 1];
-    char stack[len];
+    int n = strlen(exp);
+    char result[n + 1];
+    char stack[n];
     int j = 0;
     int top = -1;
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < n; i++) {
         char c = exp[i];
 
-        // If the scanned character is 
-        // an operand, add it to the output string.
         if (isalnum(c))
             result[j++] = c;
 
-        // If the scanned character is
-        // an ‘(‘, push it to the stack.
         else if (c == '(')
             stack[++top] = '(';
 
-        // If the scanned character is an ‘)’,
-        // pop and add to the output string from the stack 
-        // until an ‘(‘ is encountered.
         else if (c == ')') {
             while (top != -1 && stack[top] != '(') {
                 result[j++] = stack[top--];
             }
-            top--; 
+            top--;
         }
-
-        // If an operator is scanned
         else {
             while (top != -1 && (prec(c) < prec(stack[top]) ||
                                  prec(c) == prec(stack[top]))) {
@@ -57,17 +45,22 @@ void infixToPostfix(char* exp) {
         }
     }
 
-    // Pop all the remaining elements from the stack
     while (top != -1) {
         result[j++] = stack[top--];
     }
 
     result[j] = '\0';
-    printf("%s\n", result);
+    printf("Postfix Expression: %s\n", result);
 }
 
 int main() {
-    char exp[] = "a+b*(c+d)";
+    char exp[100];
+    
+    printf("Enter Infix expression: ");
+    fgets(exp, sizeof(exp), stdin);
+    
+    exp[strcspn(exp, "\n")] = '\0';
+
     infixToPostfix(exp);
     return 0;
 }

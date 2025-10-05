ea88481cc77a91cfacbc908f5f632c29be5fdb34
75hrtx <vishnu.navaneetham@outlook.com>
2025-10-05 12:36:05 +0530

    Update swap.sql

--- DBMS/DELIMITER/swap.sql
+++ DBMS/DELIMITER/swap.sql
@@ -7,3 +7,20 @@ begin
 	SET b = temp;
 end //
 delimiter ;
+
+mysql> set @x=2;
+Query OK, 0 rows affected (0.00 sec)
+
+mysql> set @y=3;
+Query OK, 0 rows affected (0.00 sec)
+
+mysql> call swap(@x,@y);
+Query OK, 0 rows affected (0.00 sec)
+
+mysql> select @x as n1, @y as n2;
++------+------+
+| n1   | n2   |
++------+------+
+|    3 |    2 |
++------+------+
+1 row in set (0.00 sec)

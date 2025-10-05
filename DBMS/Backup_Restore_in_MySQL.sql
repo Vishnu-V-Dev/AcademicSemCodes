63248278695f1dee909d0232c9d553ba634a0a99
75hrtx <vishnu.navaneetham@outlook.com>
2025-10-05 12:45:14 +0530

    Create Backup_Restore_in_MySQL.sql

---
+++ DBMS/Backup_Restore_in_MySQL.sql
@@ -0,0 +1,42 @@
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| CompanyDB          |
+| DB1                |
+| ExampleDB          |
+| LabDB              |
+| SchoolDB           |
+| ShopDB             |
+| SmallJoinDB        |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+11 rows in set (0.02 sec)
+
+[In Linux  Shell Termnal]:~$ sudo mysqldump -u root -p DB1 > backup01.sql
+
+mysql> show databases; --Dropped DB1
++--------------------+
+| Database           |
++--------------------+
+| CompanyDB          |
+| ExampleDB          |
+| LabDB              |
+| SchoolDB           |
+| ShopDB             |
+| SmallJoinDB        |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+10 rows in set (0.01 sec)
+
+mysql> create database DB1;
+Query OK, 1 row affected (0.03 sec)
+
+[In Linux  Shell Termnal]:~$ sudo mysql -u root -p DB1 < backup01.sql --backup done
+

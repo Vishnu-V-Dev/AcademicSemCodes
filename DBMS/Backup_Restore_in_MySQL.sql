ea9eb34191833f40115deabe87d71b763bf9af43
75hrtx <vishnu.navaneetham@outlook.com>
2025-10-05 12:56:25 +0530

    Update Backup_Restore_in_MySQL.sql

--- DBMS/Backup_Restore_in_MySQL.sql
+++ DBMS/Backup_Restore_in_MySQL.sql
@@ -74,3 +74,69 @@ mysql> show tables;
 1 row in set (0.00 sec)
 
 --Backup of Multiple Databases
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| CompanyDB          |
+| DB1                |
+| DB2                |
+| ExampleDB          |
+| LabDB              |
+| SchoolDB           |
+| ShopDB             |
+| SmallJoinDB        |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+12 rows in set (0.01 sec)
+
+[In Linux  Shell Termnal]:~$ sudo mysqldump -u root -p --databases DB1 DB2 > multi_db_backup.sql
+
+mysql> drop database DB1;
+Query OK, 1 row affected (0.07 sec)
+
+mysql> drop database DB2;
+Query OK, 0 rows affected (0.02 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| CompanyDB          |
+| ExampleDB          |
+| LabDB              |
+| SchoolDB           |
+| ShopDB             |
+| SmallJoinDB        |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+10 rows in set (0.00 sec)
+
+[In Linux  Shell Termnal]:~$ sudo mysql -u root -p < multi_db_backup.sql
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| CompanyDB          |
+| DB1                |
+| DB2                |
+| ExampleDB          |
+| LabDB              |
+| SchoolDB           |
+| ShopDB             |
+| SmallJoinDB        |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+12 rows in set (0.00 sec)
+
+--Backup of All Databases available

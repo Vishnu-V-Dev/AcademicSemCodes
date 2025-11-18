3320cb59cd66024dccdd1e9ee4b029cef6414289
75hrtx <vishnu.navaneetham@outlook.com>
2025-11-18 19:20:17 +0530

    Update PassTwo.c

--- PassTwo.c
+++ PassTwo.c
@@ -1,59 +1,45 @@
-Pass 2:
-
-begin
-    read first input line  {from intermediate file}
-
-    if OPCODE = 'START' then
-        begin
-            write listing line
-            read next input line
-        end  {if START}
-
-    write Header record to object program
-    initialize first Text record
-
-    while OPCODE != 'END' do
-        begin
-            if this is not a comment line then
-                begin
-                    search OPTAB for OPCODE
-
-                    if OPCODE found in OPTAB then
-                        begin
-                            if there is a symbol in OPERAND field then
-                                begin
-                                    search SYMTAB for OPERAND
-                                    if found then
-                                        store symbol value as operand address
-                                    else
-                                        begin
-                                            store 0 as operand address
-                                            set error flag  {undefined symbol}
-                                        end
-                                end
-                            else
-                                store 0 as operand address
-
-                            assemble the object code instruction
-                        end  {opcode found}
-
-                    else if (OPCODE = 'BYTE') or (OPCODE = 'WORD') then
-                        convert constant to object code
-
-                    if object code will not fit into current Text record then
-                        begin
-                            write Text record to object program
-                            initialize new Text record
-                        end
-
-                    add object code to Text record
-                end  {if not comment}
-
-            write listing
-            read next input line
-        end  {while not END}
-
-    write last Text record to object program
-    write End record to object program
-    write last listing line
-end  {Pass 2}
+Algorithm_PASS_2 {
+    
+    Read the first input line from intermediate file
+    If OPCODE = 'START' {
+        Write the line into assembly listing
+        Read next input line 
+    }
+    Write Header records to object program
+    Initialize first Text record
+    
+    While OPCODE != 'END' do {
+        
+        If this is not a comment line {
+            
+            Search OPTAB for OPCODE
+            If found {
+                
+                If there is a symbol in OPERAND field {
+                    Search SYMTAB for operand
+                    If found
+                        Store symbol value as operand address
+                    Else
+                        Set error flag
+                }
+                Else 
+                    Set 0 as operand address
+                Assemble the object code instruction
+                
+            }
+            Else if OPCODE = 'BYTE' or 'WORD'
+                Convert constant to object code 
+            If object code will not fit into the current Text record {
+                
+                Write Text record into object program
+                Initialize new Text record 
+            }
+            Add object code to Text record 
+        }
+        Write into assembly listing along with object code
+        Read next input line 
+    }
+    Write last Text record to object program
+    Write End record to object program
+    Write last listing line 
+}

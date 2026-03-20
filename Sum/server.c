028d7aa4c01680ceb9c498cd8bec0506b989b35d
75hrtx <vishnu.navaneetham@outlook.com>
2026-03-20 10:33:45 +0530

    Refactor server socket implementation
    Refactored server code to simplify socket handling and improve readability.

--- Sum/server.c
+++ Sum/server.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
 
-int main()
-{
-    int s, ns;
-    struct sockaddr_in a;
-    socklen_t l;
-    int n1, n2, sum;
+int main() {
+	int wS,nS;
+	struct sockaddr_in a;
+	socklen_t l;
+	int n1,n2,sum;
+	
+	wS = socket(AF_INET,SOCK_STREAM,0);
+	
+	a.sin_family = AF_INET;
+	a.sin_port = htons(7891);
+	a.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	bind(wS,(struct sockaddr *)&a,sizeof(a));
+	
+	listen(wS,5);
+	printf("listening...\n");
+	
+	l = sizeof(a);
+	
+	nS = accept(wS,(struct sockaddr *)&a,&l);
+	
+	recv(nS, &n1, sizeof(n1),0);
+	recv(nS, &n2, sizeof(n2),0);
 
-    s = socket(AF_INET, SOCK_STREAM, 0);
-
-    a.sin_family = AF_INET;
-    a.sin_port = htons(7891);
-    a.sin_addr.s_addr = INADDR_ANY;
-
-    bind(s, (struct sockaddr*)&a, sizeof(a));
-    listen(s, 5);
-
-    printf("Server Waiting...\n");
-
-    l = sizeof(a);
-
-    ns = accept(s, (struct sockaddr*)&a, &l);
-
-    recv(ns, &n1, sizeof(n1), 0);
-    recv(ns, &n2, sizeof(n2), 0);
-
-    sum = n1 + n2;
-
-    send(ns, &sum, sizeof(sum), 0);
-
-    close(ns);
-    close(s);
-
-    return 0;
+	sum = n1 + n2;
+	
+	send(nS,&sum,sizeof(sum),0);
+	
+	return 0;
 }

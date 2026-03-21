c49a4d34c0fa49cae990b64a6f9c94eaf429d2e5
75hrtx <vishnu.navaneetham@outlook.com>
2026-03-21 20:26:43 +0530

    Refactor FTP client to use new socket variable

--- FTP/client.c
+++ FTP/client.c
@@ -1,46 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main()
-{
-	int cS;
+int main() {
+	int wS;
 	char b[1024];
-	FILE *fp;
 	struct sockaddr_in sA;
-	socklen_t aS;
+	FILE *fp;
 
-	cS = socket(PF_INET,SOCK_STREAM,0);
-
+	wS = socket(AF_INET,SOCK_STREAM,0);
+	
 	sA.sin_family = AF_INET;
 	sA.sin_port = htons(7891);
 	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
 	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
-
-	aS = sizeof(sA);
-
-	connect(cS,(struct sockaddr *)&sA,aS);
-
-	fp = fopen("send_me.txt","rb");
-
-	if(!fp)
-	{
-		printf("Error: create 'send_me.txt' first\n");
-	}
-
-	int by;
-
-	while((by = fread(b,1,1024,fp)))
-	{
-		send(cS,b,by,0);
-	}
-
+	
+	connect(wS,(struct sockaddr *)&sA,sizeof(sA));
+	
+	fp = fopen("send.txt", "rb");
+	
+	int n;
+	while( (n = fread(b,1,sizeof(b),fp)) > 0 )
+		send(wS,b,n,0);
+		
 	fclose(fp);
-	close(cS);
-
+	printf("File send\n");
+	
 	return 0;
 }

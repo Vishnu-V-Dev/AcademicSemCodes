24e91627184f1391f50602988764a8bf8938bcbf
75hrtx <vishnu.navaneetham@outlook.com>
2026-03-21 20:14:35 +0530

    Refactor server code for clarity and structure

--- Multi Client/server.c
+++ Multi Client/server.c
@@ -1,55 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main()
-{
-	int wS,nS;
+int main() {
+	int wS,nS,s;
 	char b[1024];
-	int pid;
-
 	struct sockaddr_in sA;
-	struct sockaddr_storage sS;
-	socklen_t aS;
 
-	wS = socket(PF_INET,SOCK_STREAM,0);
-
+	wS = socket(AF_INET,SOCK_STREAM,0);
+	
 	sA.sin_family = AF_INET;
 	sA.sin_port = htons(7891);
 	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
 	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
-
-	bind(wS,(struct sockaddr *)&sA,sizeof(sA));
-
-	if(listen(wS,5)==0)
-		printf("listning\n");
-	else
-		printf("Error\n");
-
-	aS = sizeof(sS);
-
-	while((nS = accept(wS,(struct sockaddr *)&sS,&aS)))
-	{
-		if((pid = fork())==0)
-		{
-			while((recv(nS,b,1024,0))>0)
-			{
-				printf("message received = %s\n",b);
-				memset(b,0,1024);
+	
+	s= (bind(wS,(struct sockaddr *)&sA,sizeof(sA)));
+	if(s==0)printf("binded...\n");
+	
+	listen(wS,1);
+	printf("listening...\n");
+	
+	while((nS = accept(wS, NULL, NULL))) {
+		if (!fork()) {
+			while(recv(nS,b,sizeof(b),0) > 0) {
+				printf("Client: %s",b);
+				memset(b,0,sizeof(b));
 			}
-			close(nS);
-			exit(0);
-		}
-		else
-		{
-			close(nS);
 		}
 	}
-
-	close(wS);
 	return 0;
 }

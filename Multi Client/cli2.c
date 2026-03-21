8719a7b386bf05eb8f49e25b8b5f9a027128a5ea
75hrtx <vishnu.navaneetham@outlook.com>
2026-03-21 20:15:05 +0530

    Refactor client socket connection implementation

--- Multi Client/cli2.c
+++ Multi Client/cli2.c
@@ -1,42 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main()
-{
-	int cS;
+int main() {
+	int wS;
 	char b[1024];
 	struct sockaddr_in sA;
-	socklen_t aS;
 
-	cS = socket(PF_INET,SOCK_STREAM,0);
-
+	wS = socket(AF_INET,SOCK_STREAM,0);
+	
 	sA.sin_family = AF_INET;
 	sA.sin_port = htons(7891);
 	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
 	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
-
-	aS = sizeof(sA);
-
-	connect(cS,(struct sockaddr*)&sA,aS);
-
-	while(1)
-	{
-		printf("enter the message\n");
-		fgets(b,100,stdin);
-
-		send(cS,b,sizeof(b),0);
-
-		if(strncmp(b,"EXIT",4)==0)
-		{
+	
+	connect(wS,(struct sockaddr *)&sA,sizeof(sA));
+	
+	while(1) {
+		printf("Msg: ");
+		fgets(b,sizeof(b),stdin);
+		
+		send(wS,b,sizeof(b),0);
+		if(!strncmp(b,"EXIT",4))
 			break;
-		}
 	}
-
-	close(cS);
+	
 	return 0;
 }

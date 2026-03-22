756a2aa8f8fac0bd6a0301b9b5a5bbd145bb26a1
75hrtx <vishnu.navaneetham@outlook.com>
2026-03-22 16:05:44 +0530

    Refactor SMTP client to use a single socket

--- SMTP/client.c
+++ SMTP/client.c
@@ -1,59 +1,45 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-#include<sys/socket.h>
 #include<arpa/inet.h>
-#include<netinet/in.h>
 #include<unistd.h>
 
-int main()
-{
-	int cS;
-	char b[1024];
-	struct sockaddr_in sA;
-	socklen_t aS;
+int main(){
+ int wS;
+ char b[1024];
+ struct sockaddr_in sA;
 
-	cS = socket(PF_INET,SOCK_STREAM,0);
+ wS=socket(AF_INET,SOCK_STREAM,0);
 
-	sA.sin_family = AF_INET;
-	sA.sin_port = htons(7891);
-	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
-	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
+ sA.sin_family=AF_INET;
+ sA.sin_port=htons(7891);
+ sA.sin_addr.s_addr=inet_addr("127.0.0.1");
 
-	aS = sizeof(sA);
+ connect(wS,(struct sockaddr*)&sA,sizeof(sA));
 
-	connect(cS,(struct sockaddr *)&sA,aS);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	recv(cS,b,sizeof(b),0);
-	printf("server = %s",b);
+ send(wS,"HELO a\n",7,0);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	send(cS,"HELO:<localhost>",16,0);
-	memset(b,0,1024);
-	recv(cS,b,1024,0);
-	printf("server = %s",b);
+ send(wS,"MAIL a\n",7,0);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	send(cS,"MAIL FROM:<a@gmail.com>",23,0);
-	memset(b,0,1024);
-	recv(cS,b,1024,0);
-	printf("server = %s",b);
+ send(wS,"RCPT b\n",7,0);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	send(cS,"RCPT TO:<b@gmail.com>",21,0);
-	memset(b,0,1024);
-	recv(cS,b,1024,0);
-	printf("server = %s",b);
+ send(wS,"DATA\n",5,0);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	send(cS,"DATA",4,0);
-	memset(b,0,1024);
-	recv(cS,b,1024,0);
-	printf("server = %s",b);
+ send(wS,"Hello\n.\n",9,0);
 
-	send(cS," helo SMTP",14,0);
+ send(wS,"QUIT\n",5,0);
+ recv(wS,b,1024,0);
+ printf("S:%s",b);
 
-	send(cS,"QUIT",4,0);
-	memset(b,0,1024);
-	recv(cS,b,1024,0);
-	printf("server = %s",b);
-
-	close(cS);
-	return 0;
+ close(wS);
 }

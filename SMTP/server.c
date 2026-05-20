797bc323ef3c9de095291db492b472622d16a25a
Vishnu V Dev <vishnu.navaneetham@outlook.com>
2026-05-20 20:48:31 +0530

    Update server.c

--- SMTP/server.c
+++ SMTP/server.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 int main() {
-	int wS,nS;
+	int wS,nS,s;
 	char b[1024];
 	struct sockaddr_in sA;
+	socklen_t aS;
 	
 	wS = socket(AF_INET,SOCK_STREAM,0);
 	
@@ -14,41 +18,46 @@ int main() {
 	sA.sin_port = htons(7891);
 	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
-	bind(wS,(struct sockaddr *)&sA,sizeof(sA));
+	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
+	
+	s = (bind(wS,(struct sockaddr *)&sA,sizeof(sA)));
+	if(s==0) printf("Bind Successful.\n");
+	
 	listen(wS,1);
-	printf("listening...\n");
+	printf("Listening...\n");
 	
-	nS = accept(wS,NULL,NULL);
+	aS = sizeof(sA);
+	nS = accept(wS,(struct sockaddr *)&sA,&aS);
 	
-	send(nS,"220 SMTP\n",9,0);
+	send(nS,"220 SMTP Server Ready\r\n",strlen("220 SMTP Server Ready\r\n"),0);
 	
 	while(1) {
-		memset(b,0,1024);
-		recv(nS,b,1024,0);
-		printf("C: %s",b);
+		memset(b,0,sizeof(b));
 		
-		if(!strncmp(b,"HELO",4))
-			send(nS,"250 OK\n",6,0);
+		recv(nS,b,sizeof(b),0);
+		printf("Client: %s",b);
 		
-		else if(!strncmp(b,"MAIL",4))
-			send(nS,"250 OK\n",6,0);
+		if(strncmp(b,"HELO",strlen("HELO")) == 0)
+			send(nS,"250 Hello\r\n",strlen("250 Hello\r\n"),0);
 			
-		else if(!strncmp(b,"RCPT",4))
-			send(nS,"250 OK\n",6,0);
+		else if(strncmp(b,"MAIL FROM",strlen("MAIL FROM")) == 0)
+			send(nS,"250 OK\r\n",strlen("250 OK\r\n"),0);
+		
+		else if(strncmp(b,"RCPT TO",strlen("RCPT TO")) == 0)
+			send(nS,"250 OK\r\n",strlen("250 OK\r\n"),0);
 			
-		else if(!strncmp(b,"DATA",4))
-			send(nS,"354 OK\n",6,0);
+		else if(strncmp(b,"DATA",strlen("DATA")) == 0)
+			send(nS,"354 End data with <CR><LF>.<CR><LF>\r\n",strlen("354 End data with <CR><LF><CR><LF>\r\n"),0);
 			
-		else if(!strncmp(b,".",1))
-			send(nS,"250 OK\n",6,0);
+		else if(strncmp(b,".",strlen(".")) == 0)
+			send(nS,"250 Message accepted\r\n",strlen("250 Message accepted\r\n"),0);
 			
-		else if(!strncmp(b,"QUIT",4)) {
-			send(nS,"221 Bye\n",8,0);
+		else if(strncmp(b,"QUIT",strlen("QUIT")) == 0) {
+			send(nS,"221 Bye\r\n",strlen("221 Bye\r\n"),0);
 			break;
 		}
-		
 	}
-	close(nS);
-	close(wS);
 	
+	
+	return 0;
 }

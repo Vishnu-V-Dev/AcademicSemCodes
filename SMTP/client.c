00c0f085f92b451adefe02b1d9db13f919c26558
Vishnu V Dev <vishnu.navaneetham@outlook.com>
2026-05-20 20:48:48 +0530

    Refactor SMTP client for better readability and structure

--- SMTP/client.c
+++ SMTP/client.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
-#include<string.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 int main() {
 	int wS;
 	char b[1024];
 	struct sockaddr_in sA;
+	socklen_t aS;
 	
 	wS = socket(AF_INET,SOCK_STREAM,0);
 	
@@ -14,34 +18,46 @@ int main() {
 	sA.sin_port = htons(7891);
 	sA.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
-	connect(wS,(struct sockaddr *)&sA,sizeof(sA));
-	int n;
-	n = (recv(wS,b,1024,0));
-	b[n] = '\0';
-	printf("S: %s",b);
+	memset(sA.sin_zero,'\0',sizeof(sA.sin_zero));
 	
-	send(wS,"HELO a\n",7,0);
-	recv(wS,b,1024,0);
-	printf("S: %s",b);
+	aS = sizeof(sA);
+	connect(wS,(struct sockaddr *)&sA,aS);
 	
-	send(wS,"MAIL a\n",7,0);
-	recv(wS,b,1024,0);
-	printf("S: %s",b);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("Server: %s",b);
 	
-	send(wS,"RCPT b\n",7,0);
-	recv(wS,b,1024,0);
-	printf("S: %s",b);
+	send(wS,"HELO localhost\r\n",strlen("HELO localhost\r\n"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
 	
-	send(wS,"DATA\n",5,0);
-	recv(wS,b,1024,0);
-	printf("S: %s",b);
+	send(wS,"MAIL FROM:<a@test.com>\r\n",strlen("MAIL FROM:<a@test.com>\r\n"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
 	
-	send(wS,"Hello\n.\n",9,0);
+	send(wS,"RCPT TO:<b@test.com>\r\n",strlen("RCPT TO:<b@test.com>\r\n"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
 	
-	send(wS,"QUIT\n",5,0);
-	recv(wS,b,1024,0);
-	printf("S: %s",b);
+	send(wS,"DATA",strlen("DATA"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
 	
-	close(wS);
+	send(wS,"Hello SMTP\r\n",strlen("Hello SMTP\r\n"),0);;
 	
+	send(wS,".\r\n",strlen(".\r\n"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
+	
+	send(wS,"QUIT\r\n",strlen("QUIT\r\n"),0);
+	memset(b,0,sizeof(b));
+	recv(wS,b,sizeof(b),0);
+	printf("%s",b);
+	
+	return 0;
 }
